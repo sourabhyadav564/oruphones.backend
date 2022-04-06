42a1475e59698c33c1e63fc7c4eb98f15f8af2bb
Aman Khandelwal <aman@zenro.co.jp>
2022-04-06 11:20:45 +0530

    minor patches

--- routes/device/image_upload_routes.js
+++ routes/device/image_upload_routes.js
@@ -11,10 +11,9 @@ const unlinkFile = util.promisify(fs.unlink);
 const multer = require("multer");
 const { uploadFile, getFileStream } = require("../../src/s3");
 
-
 const storage = multer.diskStorage({
   destination: function (req, file, next) {
-    next(null, "/uploads/");
+    next(null, "./uploads/");
   },
   filename: function (req, file, next) {
     next(null, Date.now() + "-" + file.originalname);
@@ -22,7 +21,11 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, next) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/jpg") {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
     next(null, true);
   } else {
     next(new Error("file type not supported"), false);
@@ -43,11 +46,26 @@ router.get("/uploadimage/:key", (req, res) => {
 
 router.post("/uploadimage", upload.single("file"), async (req, res) => {
   const file = req.file;
-  console.log(file);
-  const result = await uploadFile(file);
-  await unlinkFile(file.path);
-  console.log(result);
-  res.send({ imagePath: `${result.Location}`, imageKey: `${result.Key}` });
-}); 
+  // console.log(file);
+  try {
+    const result = await uploadFile(file);
+    // console.log(result);
+    await unlinkFile(file.path);
+    const dataObject = {
+      imagePath: `${result.Location}`,
+      thumbnailImagePath: `${result.Location}`,
+      imageKey: `${result.Key}`,
+    };
+    res.status(200).json({
+      reason: "Image uploaded successfully",
+      statusCode: 201,
+      status: "SUCCESS",
+      dataObject,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
