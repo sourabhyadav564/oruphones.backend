a0e14a7f30e88162701307d0b737d68ef8005e9d
Aman Khandelwal <aman@zenro.co.jp>
2022-04-07 11:21:51 +0530

    get client ip address

--- routes/master/master_brand_routes.js
+++ routes/master/master_brand_routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const requestIp = require("request-ip");
+var getIP = require('ipware')().get_ip;
 
 require("../../src/database/connection");
 const brandModal = require("../../src/database/modals/master/master_brands");
 
 router.get("/brands", async (req, res) => {
   try {
+    var clientIp = requestIp.getClientIp(req);
+    var ipInfo = getIP(req);
     const dataObject = await brandModal.find();
     res
       .status(200)
@@ -13,6 +17,9 @@ router.get("/brands", async (req, res) => {
         reason: "Brands found",
         statusCode: 200,
         status: "SUCCESS",
+        clientIp: clientIp,
+        newIp: req.socket.remoteAddress,
+        ipInfo: ipInfo,
         dataObject,
       });
   } catch (error) {
