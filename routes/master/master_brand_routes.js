5d0d5e70422bddad965992e63d46883360686386
Aman Khandelwal <aman@zenro.co.jp>
2022-04-07 11:39:24 +0530

    new patches

--- routes/master/master_brand_routes.js
+++ routes/master/master_brand_routes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const requestIp = require("request-ip");
-var getIP = require('ipware')().get_ip;
-var imei = require('node-imei');
 
 require("../../src/database/connection");
 const brandModal = require("../../src/database/modals/master/master_brands");
 
 router.get("/brands", async (req, res) => {
-  var IMEI= new imei();
   try {
-    var clientIp = requestIp.getClientIp(req);
-    var ipInfo = getIP(req);
     const dataObject = await brandModal.find();
     res
       .status(200)
@@ -19,13 +13,7 @@ router.get("/brands", async (req, res) => {
         reason: "Brands found",
         statusCode: 200,
         status: "SUCCESS",
-        clientIp: clientIp,
-        newIp: req.socket.remoteAddress,
-        IMEI: [
-          IMEI.random(),
-          IMEI.device("OnePlus","OnePlus 8T"),
-        ],
-        ipInfo: ipInfo,
+        clientIp: req.socket.remoteAddress,
         dataObject,
       });
   } catch (error) {
