b221a4833aa8c1d9c82c9140d863c152acdc591c
Aman Khandelwal <aman@zenro.co.jp>
2022-05-02 19:07:18 +0530

    new change

--- src/database/modals/device/save_listing_device.js
+++ src/database/modals/device/save_listing_device.js
@@ -48,12 +48,12 @@ const saveListingSchema = new mongoose.Schema({
         type: [{
             fullImage: {
                 type: String,
-                default: "NA"
+                // default: "https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/apple/mbr_Apple_iPhone_12_mini.png"
             },
-            default: [{
-                type: String,
-                default: "https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/apple/mbr_Apple_iPhone_12_mini.png"
-            }],
+        }],
+        default: [{
+            type: String,
+            default: "https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/apple/mbr_Apple_iPhone_12_mini.png"
         }],
     },
     imei: {
