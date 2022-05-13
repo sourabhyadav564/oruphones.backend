25dee35dae890427f3704d33bde2275385c6f038
Aman Khandelwal <aman@zenro.co.jp>
2022-05-13 17:59:19 +0530

    empty commit

---
+++ routes/device/get_external_source_data_routes.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+
+require("../../src/database/connection");
+const scrappedModal = require("../../src/database/modals/others/scrapped_models");
+
+router.post("/price/externalsellsource", async (req, res) => {
+  const deviceStorage = req.body.deviceStorage;
+  const make = req.body.make;
+  const marketingName = req.body.marketingName;
+
+  const VENDORS = {
+    6: "Amazon",
+    7: "Quikr",
+    8: "Cashify",
+    9: "2Gud",
+    10: "Budli",
+    11: "Paytm",
+    12: "Yaantra",
+    13: "Shopcluse",
+    14: "Sahivalue",
+    15: "Xtracover",
+    16: "Mobigarage",
+    17: "Instacash",
+    18: "Cashforphone",
+    19: "Recycledevice",
+    20: "Quickmobile",
+    21: "Buyblynk",
+    22: "Electronicbazaar",
+  };
+
+  try {
+    const listings = await scrappedModal.find({
+      type: "sell",
+      storage: deviceStorage,
+      model_name: marketingName,
+    });
+
+    if (!listings.length) {
+      res.status(200).json({
+        reason: "Listing not found",
+        statusCode: 200,
+        status: "SUCCESS",
+        dataObject: [],
+      });
+    } else {
+      let filterData = {};
+      let finalDataArray = [];
+      listings.forEach(async (element) => {
+        let vendorName = VENDORS[element.vendor_id];
+        let vendorImage = `https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/vendors/${vendorName
+          .toString()
+          .toLowerCase()}_logo.png`;
+        filterData["externalSourcePrice"] = element.price;
+        filterData["externalSourceImage"] = vendorImage;
+
+        finalDataArray.push(filterData);
+      });
+      res.status(200).json({
+        reason: "Listing found",
+        statusCode: 200,
+        status: "SUCCESS",
+        dataObject: finalDataArray,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
