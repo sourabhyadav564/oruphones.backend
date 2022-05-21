ff89182ecb8d3bfce9717922aecb28a123cec39b
Aman Khandelwal <aman@zenro.co.jp>
2022-05-21 17:25:32 +0530

    minor bug fixes

--- routes/diagnostics/diagnostics_config_routes.js
+++ routes/diagnostics/diagnostics_config_routes.js
@@ -270,7 +270,7 @@ router.post("/grade/price", async (req, res) => {
   const deviceFinalGrade = req.body.deviceFinalGrade;
   const deviceFunctionalGrade = req.body.deviceFunctionalGrade;
   // fs.writeFileSync(`${listingId}.json`, JSON.stringify(req.body));
-  const saveData = res.body.saveData;
+  const saveData = req.body.saveData;
   console.log("saveData key", saveData);
 
   // PASS
