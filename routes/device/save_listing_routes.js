4787aba53106ca0d1dcf66ac3b7ec18970305944
Aman Khandelwal <aman@zenro.co.jp>
2022-05-21 15:15:51 +0530

    removing duplicate external source price main app

--- routes/device/save_listing_routes.js
+++ routes/device/save_listing_routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const moment = require("moment");
 const saveRequestModal = require("../../src/database/modals/device/request_verification_save");
 
-require("../../src/database/connection");
+// require("../../src/database/connection");
 const saveListingModal = require("../../src/database/modals/device/save_listing_device");
 const createUserModal = require("../../src/database/modals/login/login_create_user");
-// const scrappedModal = require("../src/database/modals/others/scrapped_models");
+const scrappedModal = require("../../src/database/modals/others/scrapped_models");
 const connection = require("../../src/database/mysql_connection");
 
 const logEvent = require("../../src/middleware/event_logging");
@@ -502,8 +502,8 @@ router.post("/listing/detailwithuserinfo", async (req, res) => {
       });
       return;
     } else {
-      let query =
-        "select * from `web_scraper_modelwisescraping` where created_at > now() - interval 10 day;select * from `web_scraper_model`;";
+      // let query =
+      //   "select * from `web_scraper_modelwisescraping` where created_at > now() - interval 10 day;select * from `web_scraper_model`;";
 
       const VENDORS = {
         6: "Amazon",
@@ -527,70 +527,90 @@ router.post("/listing/detailwithuserinfo", async (req, res) => {
 
       const externalSource = [];
 
-      connection.query(query, [2, 1], async (err, results, fields) => {
-        if (err) {
-          console.log(err);
-        } else {
-          let models = results[1];
-          let scrappedModels = results[0];
-          let selectdModels = [];
-          // let minPrice;
-          // let maxPrice;
-          let itemId = "";
-          const make = await getListing.make;
-          const marketingname = await getListing.marketingName;
-          const condition = await getListing.deviceCondition;
-          const storage = await getListing.deviceStorage
-            .split(" ")[0]
-            .toString();
-          let leastSellingPrice;
+      let scrappedModels = await scrappedModal.find({
+        model_name: getListing?.marketingName,
+        storage: getListing?.deviceStorage,
+        type: "buy",
+      });
 
-          models.forEach((item, index) => {
-            if (item.name === marketingname) {
-              itemId = item.id;
-              return;
-            }
-          });
+      console.log("scrappedModels", scrappedModels);
+      console.log("scrappedModel1", getListing?.marketingName);
+      console.log("scrappedModel2", getListing?.deviceStorage);
 
-          scrappedModels.forEach((item, index) => {
-            if (
-              item.model_id === itemId &&
-              item.mobiru_condition === condition &&
-              item.storage === parseInt(storage)
-            ) {
-              vendorName = VENDORS[item.vendor_id];
-              vendorImage = `https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/vendors/${vendorName
-                .toString()
-                .toLowerCase()}_logo.png`;
-              let vendorObject = {
-                externalSourcePrice: item.price,
-                externalSourceImage: vendorImage,
-              };
-              console.log("vendorObject", vendorObject);
-              selectdModels.push(vendorObject);
-            }
-          });
+      // connection.query(query, [2, 1], async (err, results, fields) => {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      // let models = results[1];
+      // let scrappedModels = results[0];
+      let selectdModels = [];
+      // let minPrice;
+      // let maxPrice;
+      let itemId = "";
+      // const make = await getListing.make;
+      const marketingname = getListing.marketingName;
+      const condition = getListing.deviceCondition;
+      const storage = getListing.deviceStorage;
+      // .split(" ")[0]
+      // .toString();
+      let leastSellingPrice;
 
-          if (selectdModels.length > 0) {
-            // leastSellingPrice = Math.max(...selectdModels);
-            externalSource.push(selectdModels[0]); //TODO: Need to remove the duplicate objects. Objects from the rarest.
-            externalSource.push(selectdModels[1]); //TODO: Need to remove the duplicate objects. Objects from the rarest.
+      // models.forEach((item, index) => {
+      //   if (item.name === marketingname) {
+      //     itemId = item.id;
+      //     return;
+      //   }
+      // });
+
+      console.log("marketingname", marketingname);
+      console.log("condition", condition);
+      console.log("storage", storage);
+
+      let pushedVendors = [];
+
+      scrappedModels.forEach((item, index) => {
+        console.log("itemId", item);
+        if (
+          item.model_name === marketingname &&
+          item.mobiru_condition === condition &&
+          item.storage === storage
+        ) {
+          console.log("item", item);
+          vendorName = VENDORS[item.vendor_id];
+          vendorImage = `https://zenrodeviceimages.s3.us-west-2.amazonaws.com/mobiru/product/mobiledevices/img/vendors/${vendorName
+            .toString()
+            .toLowerCase()}_logo.png`;
+          let vendorObject = {
+            externalSourcePrice: item.price,
+            externalSourceImage: vendorImage,
+          };
+          if (!pushedVendors.includes(vendorName)) {
+            console.log("vendorObject", vendorObject);
+            selectdModels.push(vendorObject);
+            pushedVendors.push(vendorName);
           }
-
-          let dataObject = { externalSource, ...getListing._doc };
-          // console.log("externalSource", dataObject);
-
-          // if(externalSource.length > 0) {
-          res.status(200).json({
-            reason: "Listing updated successfully",
-            statusCode: 200,
-            status: "SUCCESS",
-            dataObject,
-          });
         }
-        // }
+      });
+
+      if (selectdModels.length > 0) {
+        // leastSellingPrice = Math.max(...selectdModels);
+        externalSource.push(...selectdModels); //TODO: Need to remove the duplicate objects. Objects from the rarest.
+      }
+
+      let dataObject = { externalSource, ...getListing._doc };
+      // console.log("externalSource", dataObject);
+
+      // if(externalSource.length > 0) {
+      res.status(200).json({
+        reason: "Listing updated successfully",
+        statusCode: 200,
+        status: "SUCCESS",
+        dataObject,
       });
     }
+    // }
+    // });
+    // }
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
