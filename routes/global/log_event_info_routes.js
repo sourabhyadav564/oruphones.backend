316744e2b25a4a55666852657c4bcf2b04d0ebd1
Aman Khandelwal <aman@zenro.co.jp>
2022-05-23 14:10:05 +0530

    adding logeventinfo for login as a guest functiona

--- routes/global/log_event_info_routes.js
+++ routes/global/log_event_info_routes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 const logEvent = require("../../src/middleware/event_logging");
 
-router.post("/logeventinfo", logEvent, async (req, res) => {
-
+router.get("/logeventinfo", logEvent, async (req, res) => {
   try {
     res.status(200).send({
         status: "SUCCESS",
