044a4ec0f756972190fc905b982cf73135bca791
Aman Khandelwal <aman@zenro.co.jp>
2022-06-27 18:54:50 +0530

    handling char cases for brand name

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(cors(corsOptions));
 
 let schedule = require('node-schedule');
 
-schedule.scheduleJob('07 20 * * * ', function(){
+schedule.scheduleJob('54 16 * * * ', function(){
   console.log('The answer to life, the universe, and everything!');
   // start_migration();
   // start_migrating_external_source_buy();
