1ea7f837edcea34bb37063ab4db8a605e041ae5a
Aman Khandelwal <aman@zenro.co.jp>
2022-07-06 20:01:31 +0530

    grant access to guest for event logging

--- src/middleware/event_logging.js
+++ src/middleware/event_logging.js
@@ -7,48 +7,32 @@ const logEvent = async (req, res, next) => {
   const srcFrom = req.headers.srcfrom;
   const sessionId = req.headers.sessionid;
 
-  const getEvent = await eventModal.findOne({sessionId: sessionId});
+  const getEvent = await eventModal.findOne({ sessionId: sessionId });
   const currentTime = moment(Date.now()).format("LTS");
-  const expirationTime = moment(getEvent?.createdAt?.setHours(getEvent?.createdAt.getHours() + 4)).format("LTS");
+  const expirationTime = moment(
+    getEvent?.createdAt?.setHours(getEvent?.createdAt.getHours() + 4)
+  ).format("LTS");
 
   try {
     if (getEvent) {
+      const eventData = getEvent.events;
+      console.log("eventData", eventData);
 
-        const eventData = getEvent.events;
-
-        console.log("eventData", eventData);
-
-        // if (userUniqueId == "Guest") {
-        //   const updateEvent = await eventModal.findByIdAndUpdate(
-        //     getEvent._id,
-        //     {
-        //       $push: {
-        //         events: {
-        //           eventName: events,
-        //         }
-        //       }
-        //     },{ new: true }
-        //   )
-        //   console.log("updateEvent", updateEvent);
-        // }
-
-        // else if (userUniqueId !== "Guest" && userUniqueId !== getEvent.userUniqueId) {
-        if (userUniqueId !== getEvent.userUniqueId) {
-          const updateEvent = await eventModal.findByIdAndUpdate(
-            getEvent._id,
-            {
-              $push: {
-                events: {
-                  eventName: events,
-                }
-              }
-            },{ new: true }
-          )
-          console.log("updateEvent", updateEvent);
-        }
-
-        next();
-      // }
+      if (userUniqueId === getEvent.userUniqueId || userUniqueId === "Guest") {
+        const updateEvent = await eventModal.findByIdAndUpdate(
+          getEvent._id,
+          {
+            $push: {
+              events: {
+                eventName: events,
+              },
+            },
+          },
+          { new: true }
+        );
+        console.log("updateEvent", updateEvent);
+      } 
+      next();
     } else {
       res.status(301).send({
         status: "SESSION_INVALID",
@@ -57,7 +41,6 @@ const logEvent = async (req, res, next) => {
       });
       return;
     }
-
   } catch (error) {
     console.log(error);
   }
