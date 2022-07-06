b64457d408c4aeb7c82e9a4081fe7176ecfee494
Aman Khandelwal <aman@zenro.co.jp>
2022-07-06 14:53:37 +0530

    adding new functional login to main logs function

--- routes/diagnostics/diagnostics_config_routes.js
+++ routes/diagnostics/diagnostics_config_routes.js
@@ -1286,15 +1286,42 @@ router.post("/grade/price", async (req, res) => {
 router.post("/logDiagTransaction", async (req, res) => {
   try {
     console.log("req.body", req.body);
-    console.log("req.body.commandDetails", req.body.commandDetails);
-    const dataToBeSave = new dignosticsLogsModal(req.body);
-    const createLog = await dataToBeSave.save();
-    res.status(200).json({
-      reason: "Diagnostics logs saved successfully",
-      statusCode: 201,
-      status: "SUCCESS",
-      // data: createLog,
+    const getLogData = dignosticsLogsModal.findOne({
+      sessionId: req.body.sessionId,
     });
+
+    if (getLogData) {
+      const updateLogData = await dignosticsLogsModal.updateOne(
+        { sessionId: req.body.sessionId },
+        {
+          $set: {
+            ...req.body,
+          },
+        }
+      );
+      res.status(200).json({
+        reason: "Logs updated successfully",
+        statusCode: 200,
+        status: "SUCCESS",
+      });
+    } else {
+      const saveLogData = await dignosticsLogsModal.create({
+        ...req.body,
+      });
+      res.status(200).json({
+        reason: "Logs saved successfully",
+        statusCode: 200,
+        status: "SUCCESS",
+      });
+    }
+    // const dataToBeSave = new dignosticsLogsModal(req.body);
+    // const createLog = await dataToBeSave.save();
+    // res.status(200).json({
+    //   reason: "Diagnostics logs saved successfully",
+    //   statusCode: 201,
+    //   status: "SUCCESS",
+    //   // data: createLog,
+    // });
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
