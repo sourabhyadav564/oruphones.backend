f4260064bb2602d4c7ea3a6304978479ff2f93e0
Aman Khandelwal <aman@zenro.co.jp>
2022-08-05 17:36:06 +0530

    adding new domain to CORS

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const corsOptions = {
     "http://localhost:3001",
     "http://localhost:3002",
     "https://localhost:3003",
-    "https://localhost:5500",
+    "http://localhost:5500",
     "https://oru-phones-web.vercel.app",
     "https://oru-phones-mobile-web.vercel.app",
     "https://oru-phones-mip-portal.vercel.app",
