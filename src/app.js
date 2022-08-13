90c36d27cb533935ccc2159de03acae537810646
Aman Khandelwal <aman@zenro.co.jp>
2022-08-13 13:04:17 +0530

    adding log emails to main route

--- src/app.js
+++ src/app.js
@@ -48,15 +48,15 @@ app.use(cors(corsOptions));
 
 let schedule = require("node-schedule");
 
-schedule.scheduleJob("30 20 * * *", function () {
-  console.log("The answer to life, the universe, and everything!");
-  startCalculatingLSP();
-});
+// schedule.scheduleJob("30 20 * * *", function () {
+//   console.log("The answer to life, the universe, and everything!");
+//   startCalculatingLSP();
+// });
 
-schedule.scheduleJob("0/20 * * * *", function () {
-  console.log("The answer to life, the universe, and everything!");
-  startSavingBestDeals();
-});
+// schedule.scheduleJob("0/20 * * * *", function () {
+//   console.log("The answer to life, the universe, and everything!");
+//   startSavingBestDeals();
+// });
 
 const testRoute = require("../routes/others/test_routes");
 const brandRoute = require("../routes/master/master_brand_routes");
