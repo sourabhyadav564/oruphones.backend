eadc50ec786e3d1ed9a44f1f71ea884689a0fe03
Aman Khandelwal <aman@zenro.co.jp>
2022-09-08 13:43:58 +0530

    adding validation check to valid user middleware

--- src/middleware/valid_user.js
+++ src/middleware/valid_user.js
@@ -19,14 +19,16 @@ const validUser = async (req, res, next) => {
       });
       if (getUser) {
         next();
+      } else {
+        res.status(200).send({
+          status: "INVALID_USER",
+          statusCode: 200,
+          reason: "User not found",
+        });
       }
-    } else {
-      res.status(200).send({
-        status: "INVALID_USER",
-        statusCode: 200,
-        reason: "User not found",
-      });
       return;
+    } else {
+      next();
     }
   } catch (error) {
     console.log(error);
