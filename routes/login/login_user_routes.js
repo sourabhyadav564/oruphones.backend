5f966c107a6431c001bbd3e573078c4aa3b6aff8
Aman Khandelwal <aman@zenro.co.jp>
2022-09-23 17:47:02 +0530

    adding error handling in save listing route

--- routes/login/login_user_routes.js
+++ routes/login/login_user_routes.js
@@ -44,6 +44,7 @@ router.get("/user/details", logEvent, async (req, res) => {
 
 router.post("/user/create", logEvent, async (req, res) => {
   console.log("user body", req.body);
+  console.log("user query", req.query);
   const now = new Date();
   const currentDate = moment(now).format("L");
 
