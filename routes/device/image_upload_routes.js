6428b05429b054d7a4beef8620fd80773cf09f03
sourabhyadav564 <sourabhyadav564@gmail.com>
2022-10-07 16:49:41 +0530

    fav

--- routes/device/image_upload_routes.js
+++ routes/device/image_upload_routes.js
@@ -61,15 +61,14 @@ router.post(
       const { buffer, originalname } = req.file;
       const timestamp = new Date().toISOString();
       const ref = `${timestamp}-${originalname}.webp`;
-      const thumbnail = await sharp({
+      let thumbnail = await sharp({
         create: {
           width: 48,
           height: 48,
           channels: 4,
           background: { r: 255, g: 0, b: 0, alpha: 0.5 }
         }
-      })
-        .png()
+      }).png()
         .toBuffer();
       // await sharp(buffer)
       //   .webp({ quality: 10 })
