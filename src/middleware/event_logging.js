b1bc9d556981bdeab5b004607d29e13e3cc76575
Nishant Sharma <nishant.sharma@zenro.co.jp>
2023-03-14 12:48:12 +0530

    Event logging compulsion

--- src/middleware/event_logging.js
+++ src/middleware/event_logging.js
@@ -50,13 +50,13 @@ const logEvent = async (req, res, next) => {
         );
         next();
       } else {
-        // res.status(200).send({
-        //   status: "SESSION_INVALID",
-        //   statusCode: 200,
-        //   reason: "User session invalid",
-        // });
-        // return;
-        next();
+        res.status(200).send({
+          status: "SESSION_INVALID",
+          statusCode: 200,
+          reason: "User session invalid",
+        });
+        return;
+        // next();
       }
     } else {
       next();
