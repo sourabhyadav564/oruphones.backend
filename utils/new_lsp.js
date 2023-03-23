29b98cba9e48ef6c91e25472a664783fe97214a2
Nishant Sharma <nishant.sharma@zenro.co.jp>
2023-03-23 11:09:28 +0530

    try catch added in first func of lsp

--- utils/new_lsp.js
+++ utils/new_lsp.js
@@ -115,165 +115,176 @@ const sendMailWithAttachment = async (message) => {
 let allModelNotFound = [];
 const firstFunction = async () => {
   sendMailWithAttachment("First function started");
-  fileData = await testScrappedModal.find({}, { _id: 0 });
-  // Map all data with GSM arena data sets
-  let foundObjects = [];
-  let allModelFound = [];
-  const allgsmData = JSON.parse(fs.readFileSync("gsm_arena_filtered.json"));
-  // const fileData = await testScrappedModal.find({}, { _id: 0 });
+  try {
+    fileData = await testScrappedModal.find({}, { _id: 0 });
+    // Map all data with GSM arena data sets
+    let foundObjects = [];
+    let allModelFound = [];
+    const allgsmData = await JSON.parse(fs.readFileSync("gsm_arena_filtered.json"));
+    // const fileData = await testScrappedModal.find({}, { _id: 0 });
 
-  let gsmData = allgsmData.filter((item) => item.models.length >= 0);
-  //  && item.marketingName.includes("Samsung Galaxy S9+")
-  gsmData.forEach((element, index) => {
-    let marketingName =
-      element.marketingName.charAt(0).toUpperCase() +
-      element.marketingName.slice(1);
-    if (marketingName.includes("(")) {
-      let tempName = marketingName.split("(")[0];
-      marketingName = tempName.trim();
-    }
-    let cond = ["Like New", "Excellent", "Good", "Fair"];
-    element.storage.forEach((el) => {
-      cond.forEach((con) => {
-        let variable = fileData.filter((elm) => {
-          let mdl = elm.model_name != null ? elm.model_name.toString() : "";
-          if (mdl.includes("(")) {
-            let tempName2 = mdl.split("(")[0];
-            mdl = tempName2.trim();
-          }
-          if (mdl.includes("|")) {
-            let tempName2 = mdl.split("|")[0];
-            mdl = tempName2.trim();
-          }
-          mdl = mdl.toLowerCase().replace(/5g/g, "").trim();
-          marketingName = marketingName.toLowerCase().replace(/5g/g, "").trim();
-          // handliing poco in model name //
-
-          mdl = mdl.toLowerCase().replace(/poco/g, "xiaomi poco").trim();
-
-          // handling spaces in model name
-          mdl = mdl.toLowerCase().replace(/ /g, "").trim();
-          marketingName = marketingName.toLowerCase().replace(/ /g, "").trim();
-
-          // handling moto & motorola in model name
-          mdl = mdl
-            .toLowerCase()
-            .replace(/motorola moto/g, "motorola")
-            .trim();
-          mdl = mdl.toLowerCase().replace(/moto /g, "motorola ").trim();
-
-          marketingName = marketingName
-            .toLowerCase()
-            .replace(/motorola moto/g, "motorola")
-            .trim();
-          marketingName = marketingName
-            .toLowerCase()
-            .replace(/moto /g, "motorola ")
-            .trim();
-
-          // handling + for plus in model name
-          mdl = mdl.toLowerCase().replace(/\+/g, "plus").trim();
-          marketingName = marketingName
-            .toLowerCase()
-            .replace(/\+/g, "plus")
-            .trim();
-
-          if (
-            mdl.toLowerCase() == marketingName.toLowerCase() &&
-            el.includes(elm.storage) &&
-            elm.mobiru_condition.includes(con)
-          ) {
-            return elm;
-          } else if (
-            mdl.toLowerCase() == marketingName.toLowerCase() &&
-            elm.storage == "--" &&
-            elm.mobiru_condition.includes(con) &&
-            elm.price != null
-          ) {
-            return elm;
-          }
-          // else if (
-          //   mdl.toLowerCase().includes(marketingName.toLowerCase()) &&
-          //   el.includes(elm.storage) &&
-          //   el.includes(elm.ram) &&
-          //   elm.mobiru_condition.includes(con)
-          // ) {
-          //   return elm;
-          // }
-        });
-        if (variable.length > 0) {
-          variable = variable.filter(
-            (value, index, self) =>
-              index ===
-              self.findIndex(
-                (t) =>
-                  t.model_name === value.model_name &&
-                  t.storage === value.storage &&
-                  t.ram === value.ram &&
-                  t.mobiru_condition === value.mobiru_condition &&
-                  t.type === value.type &&
-                  t.vendor_id === value.vendor_id
-              )
-          );
-
-          allModelFound.push(marketingName);
-          variable.forEach((elm) => {
-            // let elm = variable[0];
-            let make = element.make;
-            let model = element.marketingName;
-            // let model_id = elm.model_id;
-            let storage = elm.storage
-              ? elm.storage.toString().includes("GB")
-                ? elm.storage
-                : `${elm.storage} GB`
-              : "--";
-            let ram = elm.ram
-              ? elm.ram.toString().includes("GB")
-                ? elm.ram
-                : `${elm.ram} GB`
-              : "--";
-            let condition = con;
-            let tempPrice = elm.price != null ? elm.price.toString() : "";
-            if (tempPrice.includes(".")) {
-              tempPrice = tempPrice.toString().split(".")[0].toString();
+    let gsmData = allgsmData.filter((item) => item.models.length >= 0);
+    //  && item.marketingName.includes("Samsung Galaxy S9+")
+    gsmData.forEach((element, index) => {
+      let marketingName =
+        element.marketingName.charAt(0).toUpperCase() +
+        element.marketingName.slice(1);
+      if (marketingName.includes("(")) {
+        let tempName = marketingName.split("(")[0];
+        marketingName = tempName.trim();
+      }
+      let cond = ["Like New", "Excellent", "Good", "Fair"];
+      element.storage.forEach((el) => {
+        cond.forEach((con) => {
+          let variable = fileData.filter((elm) => {
+            let mdl = elm.model_name != null ? elm.model_name.toString() : "";
+            if (mdl.includes("(")) {
+              let tempName2 = mdl.split("(")[0];
+              mdl = tempName2.trim();
             }
-            if (tempPrice.includes(",")) {
-              tempPrice = tempPrice.toString().replace(",", "").toString();
+            if (mdl.includes("|")) {
+              let tempName2 = mdl.split("|")[0];
+              mdl = tempName2.trim();
             }
+            mdl = mdl.toLowerCase().replace(/5g/g, "").trim();
+            marketingName = marketingName
+              .toLowerCase()
+              .replace(/5g/g, "")
+              .trim();
+            // handliing poco in model name //
 
-            let price = parseInt(tempPrice);
-            let type = elm.type;
-            let vendor_id = elm.vendor_id;
+            mdl = mdl.toLowerCase().replace(/poco/g, "xiaomi poco").trim();
 
-            let dataObj = {
-              make,
-              model,
-              storage,
-              ram,
-              condition,
-              price,
-              type,
-              vendor_id,
-            };
-            foundObjects.push(dataObj);
+            // handling spaces in model name
+            mdl = mdl.toLowerCase().replace(/ /g, "").trim();
+            marketingName = marketingName
+              .toLowerCase()
+              .replace(/ /g, "")
+              .trim();
+
+            // handling moto & motorola in model name
+            mdl = mdl
+              .toLowerCase()
+              .replace(/motorola moto/g, "motorola")
+              .trim();
+            mdl = mdl.toLowerCase().replace(/moto /g, "motorola ").trim();
+
+            marketingName = marketingName
+              .toLowerCase()
+              .replace(/motorola moto/g, "motorola")
+              .trim();
+            marketingName = marketingName
+              .toLowerCase()
+              .replace(/moto /g, "motorola ")
+              .trim();
+
+            // handling + for plus in model name
+            mdl = mdl.toLowerCase().replace(/\+/g, "plus").trim();
+            marketingName = marketingName
+              .toLowerCase()
+              .replace(/\+/g, "plus")
+              .trim();
+
+            if (
+              mdl.toLowerCase() == marketingName.toLowerCase() &&
+              el.includes(elm.storage) &&
+              elm.mobiru_condition.includes(con)
+            ) {
+              return elm;
+            } else if (
+              mdl.toLowerCase() == marketingName.toLowerCase() &&
+              elm.storage == "--" &&
+              elm.mobiru_condition.includes(con) &&
+              elm.price != null
+            ) {
+              return elm;
+            }
+            // else if (
+            //   mdl.toLowerCase().includes(marketingName.toLowerCase()) &&
+            //   el.includes(elm.storage) &&
+            //   el.includes(elm.ram) &&
+            //   elm.mobiru_condition.includes(con)
+            // ) {
+            //   return elm;
+            // }
           });
-        } else {
-          // console.log("marketingName: " +marketingName);
-          if (!allModelNotFound.includes("GSM: " + element.marketingName)) {
-            allModelNotFound.push("GSM: " + element.marketingName);
+          if (variable.length > 0) {
+            variable = variable.filter(
+              (value, index, self) =>
+                index ===
+                self.findIndex(
+                  (t) =>
+                    t.model_name === value.model_name &&
+                    t.storage === value.storage &&
+                    t.ram === value.ram &&
+                    t.mobiru_condition === value.mobiru_condition &&
+                    t.type === value.type &&
+                    t.vendor_id === value.vendor_id
+                )
+            );
+
+            allModelFound.push(marketingName);
+            variable.forEach((elm) => {
+              // let elm = variable[0];
+              let make = element.make;
+              let model = element.marketingName;
+              // let model_id = elm.model_id;
+              let storage = elm.storage
+                ? elm.storage.toString().includes("GB")
+                  ? elm.storage
+                  : `${elm.storage} GB`
+                : "--";
+              let ram = elm.ram
+                ? elm.ram.toString().includes("GB")
+                  ? elm.ram
+                  : `${elm.ram} GB`
+                : "--";
+              let condition = con;
+              let tempPrice = elm.price != null ? elm.price.toString() : "";
+              if (tempPrice.includes(".")) {
+                tempPrice = tempPrice.toString().split(".")[0].toString();
+              }
+              if (tempPrice.includes(",")) {
+                tempPrice = tempPrice.toString().replace(",", "").toString();
+              }
+
+              let price = parseInt(tempPrice);
+              let type = elm.type;
+              let vendor_id = elm.vendor_id;
+
+              let dataObj = {
+                make,
+                model,
+                storage,
+                ram,
+                condition,
+                price,
+                type,
+                vendor_id,
+              };
+              foundObjects.push(dataObj);
+            });
+          } else {
+            // console.log("marketingName: " +marketingName);
+            if (!allModelNotFound.includes("GSM: " + element.marketingName)) {
+              allModelNotFound.push("GSM: " + element.marketingName);
+            }
           }
-        }
+        });
       });
+      // console.log({ index });
+      if (index == gsmData.length - 1) {
+        // fs.writeFileSync(
+        //   "allModelNotFound.json",
+        //   JSON.stringify(allModelNotFound, null, 2)
+        // );
+        secondFunction(foundObjects);
+      }
     });
-    console.log({ index });
-    if (index == gsmData.length - 1) {
-      // fs.writeFileSync(
-      //   "allModelNotFound.json",
-      //   JSON.stringify(allModelNotFound, null, 2)
-      // );
-      secondFunction(foundObjects);
-    }
-  });
+  } catch (error) {
+    console.log(error);
+    sendMailWithAttachment("Error in first function\n\n" + error.toString());
+  }
 };
 
 const secondFunction = (foundObjects) => {
