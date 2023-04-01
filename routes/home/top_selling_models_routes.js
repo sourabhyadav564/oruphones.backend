8a61af7eda53104994504e951982d9624649c9fa
Nishant Sharma <nishant.sharma@zenro.co.jp>
2023-04-01 18:24:55 +0530

    top selling model api breakup

--- routes/home/top_selling_models_routes.js
+++ routes/home/top_selling_models_routes.js
@@ -5,74 +5,24 @@ require("../../src/database/connection");
 // const listingByMakeModal = require("../../src/database/modals/listing/listing_by_make");
 const saveListingModal = require("../../src/database/modals/device/save_listing_device");
 const logEvent = require("../../src/middleware/event_logging");
+const { newModelImages } = require("../../utils/models_util");
 
 router.get("/topselling/models", async (req, res) => {
   try {
-    // const listings = await saveListingModal.find();
+    let isLimited = req.query.isLimited || "true";
 
-    // const topModels = async () => {
-    //   let dataObject = [];
-    //   let modelVals = [];
+    switch (isLimited) {
+      case "true" || true:
+        isLimited = true;
+        break;
+      case "false" || false:
+        isLimited = false;
+        break;
+      default:
+        isLimited = true;
+        break;
+    }
 
-    //   listings.forEach(async (item, i) => {
-    //     let modelName = item.marketingName;
-    //     if (!modelVals.includes(modelName)) {
-    //       let data = {
-    //         make: item.make,
-    //         marketingName: item.marketingName,
-    //         startingFrom: item.listingPrice,
-    //         maxPrice: item.listingPrice,
-    //         imagePath: item.defaultImage.fullImage,
-    //         displayOrder: 9999,
-    //         isTopSelling: false,
-    //         quantity: 1,
-    //       };
-    //       modelVals.push(modelName);
-    //       dataObject.push(data);
-    //     } else {
-    //       let data = {};
-    //       var modObj = dataObject.filter(obj => {
-    //         return obj.marketingName === modelName;
-    //       })
-
-    //       let mObj = modObj[0];
-    //       let stf = mObj.startingFrom;
-    //       let maxP = mObj.maxPrice;
-    //       let listP = item.listingPrice;
-
-    //       let startFrom =
-    //         parseInt(stf) > parseInt(listP)
-    //           ? item.listingPrice
-    //           : mObj.startingFrom;
-    //       let mPrice =
-    //         parseInt(maxP) > parseInt(listP)
-    //           ? mObj.maxPrice
-    //           : item.listingPrice;
-
-    //       data = {
-    //         make: mObj.make,
-    //         marketingName: mObj.marketingName,
-    //         startingFrom: startFrom,
-    //         maxPrice: mPrice,
-    //         imagePath: mObj.imagePath,
-    //         displayOrder: 9999,
-    //         isTopSelling: false,
-    //         quantity: mObj.quantity + 1,
-    //       };
-    //       let objIndex = dataObject.findIndex(
-    //         (obj) => obj.marketingName === mObj.marketingName
-    //       );
-    //       dataObject[objIndex] = data;
-    //     }
-    //   });
-    //   dataObject.sort(function (a, b) { return b.quantity - a.quantity });
-    //   // return dataObject.slice(0, 50);
-    //   return dataObject;
-    // };
-
-    // let dataObject = await topModels();
-
-    // rewriting above code to get top selling models with faster response time
     let dataObject = await saveListingModal.aggregate([
       {
         $group: {
@@ -81,7 +31,10 @@ router.get("/topselling/models", async (req, res) => {
           marketingName: { $first: "$marketingName" },
           maxPrice: { $max: { $toInt: "$listingPrice" } },
           minPrice: { $min: { $toInt: "$listingPrice" } },
-          imagePath: { $first: "$defaultImage.fullImage" },
+          // imagePath: { $first: "$defaultImage.fullImage" },
+
+          // imagePath: newModelImages[{ $toLower: { $first: "$marketingName" } }],
+
           displayOrder: { $first: 9999 },
           isTopSelling: { $first: false },
           quantity: { $sum: 1 },
@@ -106,25 +59,37 @@ router.get("/topselling/models", async (req, res) => {
       { $limit: 50 },
     ]);
 
-    // let allModels = [];
-    // allModels = allModels.concat(dataObject.map((item) => item.marketingName));
+    // now add imagePath to dataObject
+    dataObject.map((item) => {
+      let modelName = item.marketingName;
+      modelName = modelName.toLowerCase().replace("+", " plus");
+      item.imagePath = newModelImages[modelName];
+    });
 
-    // let allModels = await saveListingModal.distinct("marketingName");
-    // for allModels we need count of each model
-    let allModels = await saveListingModal.aggregate([
-      {
-        $group: {
-          _id: "$marketingName",
-          make: { $first: "$make" },
-          marketingName: { $first: "$marketingName" },
-          imagePath: { $first: "$defaultImage.fullImage" },
-          displayOrder: { $first: 9999 },
-          isTopSelling: { $first: false },
-          quantity: { $sum: 1 },
+    let allModels = [];
+    if (!isLimited) {
+      allModels = await saveListingModal.aggregate([
+        {
+          $group: {
+            _id: "$marketingName",
+            make: { $first: "$make" },
+            marketingName: { $first: "$marketingName" },
+            imagePath: { $first: "$defaultImage.fullImage" },
+            displayOrder: { $first: 9999 },
+            isTopSelling: { $first: false },
+            quantity: { $sum: 1 },
+          },
         },
-      },
-      { $sort: { quantity: -1 } },
-    ]);
+        { $sort: { quantity: -1 } },
+      ]);
+
+      // now add imagePath to allModels
+      allModels.map((item) => {
+        let modelName = item.marketingName;
+        modelName = modelName.toLowerCase().replace("+", " plus");
+        item.imagePath = newModelImages[modelName];
+      });
+    }
 
     res.status(200).json({
       reason: "Listings by marketing name",
