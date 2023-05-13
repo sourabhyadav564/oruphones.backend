1abbb595dc0a0abbfb9081185b3aa40eb4eb0738
Abishek Devendran <abishekdevendran@gmail.com>
2023-05-13 11:00:50 +0530

    redis retries

--- src/controllers/listing/index.ts
+++ src/controllers/listing/index.ts
@@ -4,32 +4,17 @@ import redisClient from '@/database/redis';
 
 async function topSellingHome(req: Request, res: Response) {
 	const { location, count } = req.body;
-	// if location is India
-	if (location === 'India') {
-		// check cache for India
-		let redisResponse = await redisClient.get('India');
-		if (redisResponse !== null) {
-			res.status(200).json({ data: JSON.parse(redisResponse) });
-			return;
-		}
-		// if cache miss
-		let topSelling = await Listing.find({}).sort({ sold: -1 }).limit(count);
-		res.status(200).json({ data: topSelling });
-		// set cache for India
-		redisClient.set('India', JSON.stringify(topSelling));
-		return;
-	}
+	//check redis for location
 	let redisResponse = await redisClient.get(location);
 	if (redisResponse !== null) {
 		res.status(200).json({ data: JSON.parse(redisResponse) });
 		return;
 	}
-	// if cache miss
-	let topSelling = await Listing.find({ listingLocation: location })
-		.sort({ sold: -1 })
-		.limit(count);
+	const filter = {
+		...(location === 'India' ? {} : { listingLocation: location }),
+	};
+	const topSelling = await Listing.find(filter).sort({ sold: -1 }).limit(count);
 	res.status(200).json({ data: topSelling });
-	// set cache for India
 	await redisClient.set(location, JSON.stringify(topSelling));
 }
 
--- src/database/redis.ts
+++ src/database/redis.ts
@@ -4,10 +4,14 @@ const redisClient = createClient({
 	url: process.env.REDIS_URL,
 	socket: {
 		reconnectStrategy(retries) {
+			console.log('retries: ', retries);
 			return Math.min(retries * 100, 3000);
 		},
 	},
-});
+})
+	.on('connect', () => console.log('REDIS:connect', new Date().toJSON()))
+	.on('ready', () => console.log('REDIS:ready', new Date().toJSON()))
+	.on('error', (err) => console.error('REDIS:error', err, new Date().toJSON()));
 
 redisClient
 	.connect()
