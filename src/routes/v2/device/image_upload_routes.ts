30cf792ffbe61e816fba16c342baa407866f9762
Abishek Devendran <abishekdevendran@gmail.com>
2023-06-16 19:09:18 +0530

    allows pdf, octet, img

--- src/routes/v2/device/image_upload_routes.ts
+++ src/routes/v2/device/image_upload_routes.ts
@@ -8,13 +8,15 @@ import multerS3 from 'multer-s3-transform';
 import sharp from 'sharp';
 
 const bucketName = process.env.AWS_BUCKET_NAME;
+const regex = /^(image\/.+|application\/octet-stream|application\/pdf)$/; //tests true for image/*, application/octet-stream, and application/pdf
+const regexWithoutTransform = /^(application\/pdf)$/; //tests true for application/pdf
 
 const fileFilter = (
 	req: Request,
 	file: Express.Multer.File,
 	cb: (arg0: Error | null, arg1: boolean) => void
 ) => {
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+	if (regex.test(file.mimetype)) {
 		cb(null, true);
 	} else {
 		cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
@@ -24,13 +26,17 @@ const fileFilter = (
 const options = {
 	s3,
 	bucket: bucketName,
+	contentType: multerS3.AUTO_CONTENT_TYPE,
 	acl: 'public-read',
 	cacheControl: 'max-age=31536000',
 	limits: {
 		fileSize: 5 * 1000 * 1000, // 5 MB
 	},
 	shouldTransform: function (req: Request, file: Express.Multer.File, cb: any) {
-		cb(null, /^image/i.test(file.mimetype));
+		if (regexWithoutTransform.test(file.mimetype)) {
+			return cb(null, false);
+		}
+		return cb(null, /^image/i.test(file.mimetype));
 	},
 	transforms: [
 		{
