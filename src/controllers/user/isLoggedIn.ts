14d67c32e73e860bc67233498b61d430ae432a51
Abishek Devendran <abishekdevendran@gmail.com>
2023-06-26 11:51:55 +0530

    Removed userUniqueId from payload

--- src/controllers/user/isLoggedIn.ts
+++ src/controllers/user/isLoggedIn.ts
@@ -4,7 +4,7 @@ export default async function isLoggedIn(req: Request, res: Response) {
 	res.status(200).json({
 		isLoggedIn: req.session.user ? true : false,
 		...(req.session.user && {
-			user: req.session.user,
+			user: { ...req.session.user, userUniqueId: undefined },
 		}),
 	});
 }
