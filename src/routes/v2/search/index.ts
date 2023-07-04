0bb53117faeb2d8e85ba31875113a064eb7a6f03
vishnuoruphones <vishnu@oruphones.com>
2023-07-04 12:54:48 +0530

    added getLocation

--- src/routes/v2/search/index.ts
+++ src/routes/v2/search/index.ts
@@ -1,8 +1,10 @@
 import searchController from '@/controllers/search/index';
 import topsearchController from '@/controllers/search/topSearch';
+import getLocation from '@/controllers/search/getLocation';
 import express from 'express';
 
 const router = express.Router();
 router.post('/search', searchController);
 router.get('/topSearch', topsearchController);
+router.post('/getLocation', getLocation)
 export default router;
