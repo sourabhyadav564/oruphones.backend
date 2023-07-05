52232338afb736cb69b5d042b8a2f898c36ab30c
Abishek Devendran <abishekdevendran@gmail.com>
2023-07-05 14:15:26 +0530

    updated add listing route

--- src/controllers/listing/add.ts
+++ src/controllers/listing/add.ts
@@ -3,33 +3,35 @@ import bestDealsModal from '@/database/modals/others/best_deals_models';
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 
+
 const validator = z.object({
-	charger: z.string().min(1).max(255).optional(),
-	earphone: z.string().min(1).max(255).optional(),
-	originalBox: z.string().min(1).max(255).optional(),
-	deviceCondition: z.string().min(1).max(255).optional(),
-	deviceStorage: z.string().min(1).max(255).optional(),
-	deviceRam: z.string().min(1).max(255).optional(),
-	images: z
-		.array(
-			z.object({
-				thumbImage: z.string().min(1).max(255),
-				fullImage: z.string().min(1).max(255),
-			})
-		)
-		.optional(),
-	listingPrice: z.number().min(1).max(1000000).optional(),
-	warranty: z.string().optional(),
-	latlong: z
-		.object({
-			longitude: z.number().min(-180).max(180),
-			latitude: z.number().min(-90).max(90),
-		})
-		.optional(),
-	listingState: z.string().min(1).max(255).optional(),
-	listingLocality: z.string().min(1).max(255).optional(),
-	listingId: z.string().min(1).max(255).optional(),
+	make: z.string().min(1).max(255),
+	marketingName: z.string().min(1).max(255),
+	model: z.string().min(1).max(255),
+	charger: z.string().min(1).max(255),
+	earphone: z.string().min(1).max(255),
+	originalBox: z.string().min(1).max(255),
+	deviceCondition: z.string().min(1).max(255),
+	deviceStorage: z.string().min(1).max(255),
+	deviceRam: z.string().min(1).max(255),
+	images: z.any().array(),
+	listingPrice: z.number().min(1).max(1000000),
+	warranty: z.string(),
+	latlong: z.object({
+		longitude: z.number().min(-180).max(180),
+		latitude: z.number().min(-90).max(90),
+	}),
+	state: z.string().min(1).max(255),
+	locality: z.string().min(1).max(255),
+	listingLocation: z.string().min(1).max(255),
+	cosmetic: z.object({
+		'0': z.string().min(1).max(255),
+		'1': z.string().min(1).max(255),
+		'2': z.string().min(1).max(255),
+	}),
 });
+
+export type AddListingBody = z.infer<typeof validator>;
 
 export default async function addListing(
 	req: Request,
@@ -38,63 +40,48 @@ export default async function addListing(
 ) {
 	try {
 		const body = validator.parse(req.body);
-		const listing = await saveListingModal.findOne({
-			listingId: body.listingId,
+		const limitExceeded =
+			(await saveListingModal.find().countDocuments({
+				userUniqueId: req.session.user?.userUniqueId,
+				verified: false,
+				status: 'Active',
+			})) >= 5;
+		if (limitExceeded) {
+			return res.status(400).json({
+				message: 'You have exceeded the limit of 5 listings',
+			});
+		}
+		const duplicate = await saveListingModal.findOne({
+			userUniqueId: req.session.user?.userUniqueId,
+			marketingName: body.marketingName,
+			deviceStorage: body.deviceStorage,
+			deviceRam: body.deviceRam,
+			verified: false,
 		});
-		if (!listing || listing == null) {
-			res.status(404).json({
-				message: 'Listing not found',
+		if (duplicate) {
+			return res.status(400).json({
+				message: 'Duplicate listing',
 			});
 		}
-		if (listing?.userUniqueId !== req.session.user?.userUniqueId) {
-			res.status(403).json({
-				message: 'You are not authorized to update this listing',
-			});
-		}
+		// create a new listing in saveListingModal and BestDealsModal
 		await Promise.all([
-			saveListingModal.updateOne(
-				{
-					listingId: body.listingId,
-				},
-				{
-					...body,
-					...(body.warranty
-						? {
-								warranty: body.warranty.includes('9')
-									? 'zero'
-									: body.warranty.includes('6')
-									? 'four'
-									: body.warranty.includes('3')
-									? 'seven'
-									: 'None',
-						  }
-						: {}),
-				}
-			),
-			bestDealsModal.updateOne(
-				{
-					listingId: body.listingId,
-				},
-				{
-					...body,
-					...(body.warranty
-						? {
-								warranty: body.warranty.includes('9')
-									? 'zero'
-									: body.warranty.includes('6')
-									? 'four'
-									: body.warranty.includes('3')
-									? 'seven'
-									: 'None',
-						  }
-						: {}),
-				}
-			),
+			saveListingModal.create({
+				...body,
+				latLong: undefined,
+				location: [body.latlong.longitude, body.latlong.latitude],
+				userUniqueId: req.session.user?.userUniqueId,
+				verified: false,
+				status: 'Active',
+			}),
+			bestDealsModal.create({
+				...body,
+				latLong: undefined,
+				location: [body.latlong.longitude, body.latlong.latitude],
+				userUniqueId: req.session.user?.userUniqueId,
+				verified: false,
+				status: 'Active',
+			}),
 		]);
-
-		res.status(200).json({
-			message: 'Listing updated successfully',
-		});
 	} catch (error) {
 		next(error);
 	}
